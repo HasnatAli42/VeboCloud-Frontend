42302d00db074e3e8fa52683e0afe44f42c101b3
Hasnat Ali <hasnatali42@gmail.com>
2024-11-25 23:25:24 +0500

    added comment for deloyment

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 interface User {
   id: string;
@@ -8,12 +8,12 @@ interface User {
     role_id: number;
   };
 }
-// added comment for deployment
+// added comment for deployment again
 const useAuth = () => {
   // const [user, setUser] = useState<User | null>(null);
   // const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   // const [loading, setLoading] = useState<boolean>(true);
-  const user : User | null = null;
+  const user: User | null = null;
   const isAuthenticated = false;
   const loading = true;
 
@@ -27,7 +27,6 @@ const useAuth = () => {
     //     role_id: Number(import.meta.env.VITE_DEFAULT_USER_GROUP), // Assuming this value is set in your .env
     //   },
     // };
-
     // Simulating an async operation
     // setTimeout(() => {
     //   setUser(mockUser);
