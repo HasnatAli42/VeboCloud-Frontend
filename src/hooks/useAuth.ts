6e3b970ffb4dfa5b75e33b24b78d9ba9bf7f8abc
Hasnat Ali <hasnatali42@gmail.com>
2024-12-02 20:53:32 +0500

    deployment

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -8,7 +8,7 @@ interface User {
     role_id: number;
   };
 }
-// added comment for deployment again, 2
+// added comment for deployment again, 2, 3
 const useAuth = () => {
   // const [user, setUser] = useState<User | null>(null);
   // const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
